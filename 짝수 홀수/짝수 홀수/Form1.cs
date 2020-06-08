af693c24a8c3a866af78a91e132c6c160511b4ca
Good-jjun <jjun3671@naver.com>
2020-06-08 17:53:52 +0900

    C#_private

---
+++ 짝수 홀수/짝수 홀수/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 짝수_홀수
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num = int.Parse(textBox1.Text);
+
+            if (num % 2 == 0)
+
+                label2.Text = num + "는" + "짝수입니다";
+
+            if (num % 2 == 1)
+                label2.Text = num + "는" + " 홀수입니다";
+
+        }
+    }
+}
